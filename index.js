f41b3691664b7979c0c94ebaaacba9fed157292c
JingAn Chen <chen.jingan@icloud.com>
2022-05-18 00:11:57 +0800

    Done

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const DATA = [
 	[8.6, 5.7],
 	[8.3, 5.9],
 	[7.8, 6],
+	[7.9, 6.3],
 ];
 
 const TEMPLATE = document.querySelector("template");
@@ -34,9 +35,11 @@ const MAIN = document.querySelector("body > main");
 
 for (const data of DATA) {
 	const d = document.createRange().createContextualFragment(TEMPLATE.innerHTML);
-	const difference = Math.max(data[0] - data[1], 0);
-	d.querySelector("section > header > div").style.height = `${(data[1] - 4) * (data[1] - 4) * (data[1] - 4)}%`;
-	d.querySelector("section > footer > div").style.height = `${(difference * difference + 3) * 10}%`;
-	console.log(difference);
+	const blue = data[1] - 4;
+	const yellow = Math.max(data[0] - data[1], 0);
+	const blueP = Math.min(blue ** 2.5 + 30, 100);
+	const yellowP = Math.min((yellow ** 2 + 3) * 10, 100);
+	d.querySelector("section > header").style.height = `${blueP}%`;
+	d.querySelector("section > footer").style.height = `${yellowP}%`;
 	MAIN.appendChild(d);
 }
